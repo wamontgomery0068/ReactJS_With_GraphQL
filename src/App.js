8b7efa53f752548f34bff7cf523d363507978f04
Your Name <wamontgomery0068@gmail.com>
2019-04-02 11:57:16 -0600

    Imported the Chart Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+// Imported Components Below
+import Chart from './components/Chart';
+
+// Imported Components Above
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
-      
+        <Chart />
       </div>
     );
   }
